4898ce8efa369acaa5a1c37d306237cc5ef5e550
mrshinds <wpresign@gmail.com>
2025-04-20 10:46:59 +0900

    v15

--- src/app/issue-analysis/page.tsx
+++ src/app/issue-analysis/page.tsx
@@ -1,1 +1,12 @@
- 
+import React from 'react';
+
+const IssueAnalysisPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Issue Analysis</h1>
+      <p>This is the issue analysis page.</p>
+    </div>
+  );
+};
+
+export default IssueAnalysisPage; 
